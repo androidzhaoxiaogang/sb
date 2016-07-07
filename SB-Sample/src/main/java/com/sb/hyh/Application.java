4bd1c3cabc50ac25ab12976cf29111bb4405db1e
hong0220 <1023746826@qq.com>
2016-07-07 20:19:05 +0800

    增加一波代码

--- SB-Sample/src/main/java/com/sb/hyh/Application.java
+++ SB-Sample/src/main/java/com/sb/hyh/Application.java
@@ -3,14 +3,12 @@ package com.sb.hyh;
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@EnableAutoConfiguration
-@SpringBootApplication
 @ServletComponentScan
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
